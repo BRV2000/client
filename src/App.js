cd686a346c6acf1137141aa6302e1e1b6d341094
BRV2000 <branrv2000@gmail.com>
2023-07-11 15:39:29 -0600

    añadido de carpeta rutas y otros recursos

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import { Button } from 'semantic-ui-react'
 
 export default function App() {
   return (
     <div>
       <h1>Estamos en mi WEB</h1>
+      <div>
+      
+      <Button primary>Primary</Button>
+      <Button secondary>Secondary</Button>
+  
+      </div>
     </div>
   );
 }
