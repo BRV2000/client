5fb6ddb5ab4adc8101a5fcfe7ca40ae3ee4432ca
BRV2000 <branrv2000@gmail.com>
2023-07-24 09:53:05 -0600

    validacion del Login

--- src/components/Admin/Auth/RegisterForm/RegisterForm.js
+++ src/components/Admin/Auth/RegisterForm/RegisterForm.js
@@ -1,32 +1,65 @@
 import React,{useState} from 'react'
 import {Form} from "semantic-ui-react";
 import {useFormik} from "formik";
-import {initialValues} from "./registerForm.form";
+import {initialValues,validationSchem} from "./registerForm.form";
 import "./RegisterForm.scss";
+import * as Yup from "yup";
 
 export  function RegisterForm() {
   const [error,setError] = useState("");
 
   const formik = useFormik({
     initialValues: initialValues(),
+    validationSchema: validationSchem(),
+    validationOnChange: false,
+
     onSubmit: async (formValue) => {
       try {
+        setError("");
         console.log(formValue);
       } catch (error) {
-        console.error(error);
+        setError("Error en el Servidor");
       }
     }
   });
     return (
     <Form className='register-form' onSubmit={formik.handleSubmit}>
-        <Form.Input name="email" placeholder="Correo electronico" onChange={formik.handleChange} value={formik.values.email}/>
-        <Form.Input name="password" type='password' placeholder="Contraseña"onChange={formik.handleChange} value={formik.values.password}/>
-        <Form.Input name="repeatpassword" type='password' placeholder="Repetir Contraseña"onChange={formik.handleChange} value={formik.values.repeatPassword}/>
+        <Form.Input 
+          name="email" 
+          placeholder="Correo electronico" 
+          onChange={formik.handleChange} 
+          value={formik.values.email}
+          error={formik.errors.email}
+         />
+
+        <Form.Input 
+          name="password" 
+          type='password' 
+          placeholder="Contraseña" 
+          onChange={formik.handleChange} 
+          value={formik.values.password}
+          error={formik.errors.password}
+        />
+
+        <Form.Input 
+          name="repeatPassword" 
+          type='password' 
+          placeholder="Repetir Contraseña" 
+          onChange={formik.handleChange} 
+          value={formik.values.repeatPassword} 
+          error={formik.errors.repeatPassword} 
+        />
         
-        <Form.Checkbox name="coditionAccepted" label="He leido y acepto las politicas de privacidad"
-        onChange={(_, data) => formik.setFieldValue("coditionAccepted", data.checked)} checked={formik.values.conditionsAccepted}/>
+        <Form.Checkbox 
+          name="coditionAccepted" 
+          label="He leido y acepto las politicas de privacidad"
+          onChange={(_, data) => formik.setFieldValue("coditionAccepted", data.checked)} 
+          checked={formik.values.coditionAccepted}
+          error={formik.errors.coditionAccepted}
+        />
         
-        <Form.Button type='submit' primary fluid loading={formik.isSubmitting}>
+        <Form.Button type='submit' 
+        primary fluid loading={formik.isSubmitting}>
             Crear cuenta
         </Form.Button>
         <p className='register-form__error'>{error}</p>
--- src/components/Admin/Auth/RegisterForm/registerForm.form.js
+++ src/components/Admin/Auth/RegisterForm/registerForm.form.js
@@ -12,5 +12,8 @@ export function initialValues() {
 export function validationSchem () {
     return Yup.object ({
         email: Yup.string().email("El email no es valido").required("Campo obligatorio"),
-    })
+        password: Yup.string().required("Campo obligatorio"),
+        repeatPassword: Yup.string().required("Campo Obligatorio").oneOf([Yup.ref("password")],"Las contraseñas deben ser iguales"),
+        coditionAccepted: Yup.bool().isTrue(true),
+    });
 }
